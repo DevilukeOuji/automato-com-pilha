3aea59cce0411588e089c1b8b8292ce2bcef94f0
Deviluke <47189292+DevilukeOuji@users.noreply.github.com>
2019-07-03 16:00:22 -0300

    Update a-pilha.py

--- a-pilha.py
+++ a-pilha.py
@@ -177,12 +177,12 @@ def tratarS(gram):
         gram[elem] = str(gram[elem])
         if len(gram[elem]) == 1 and (gram[elem].islower() or gram[elem].isnumeric()):
             alf += [gram[elem]]
-        if len(gram[elem]) == 2:
+        elif len(gram[elem]) == 2 or len(gram[elem]) == 3:
             if gram[elem] not in est:
                 est += [gram[elem]]
             else:
                 est_finais += [gram[elem]]
-        if gram[elem].isupper():
+        elif gram[elem].isupper():
             L1 += [gram[elem]]
             if elem + 1 < len(gram):
                 L1 += [gram[elem + 1]]
